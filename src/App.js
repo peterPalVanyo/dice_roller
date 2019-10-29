64d53c563bdb579333b496f483112fd0b8cbf724
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-10-29 14:01:17 +0100

    set up rolldice with hard coded state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Die from './Die'
+import Rolldice from './Rolldice'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Die face='two'/>
-      <Die face='four'/>
+      <Rolldice/>
     </div>
   );
 }
