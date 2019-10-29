6bf2ce9076f2aa576d661d4f2a3cdfd138f2eb95
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-10-29 14:19:11 +0100

    the roll function in rolldice.js

--- src/Rolldice.js
+++ src/Rolldice.js
@@ -3,15 +3,25 @@ import Die from './Die'
 
 
 class Rolldice extends Component {
+    static defaultProps = {
+        sides: ['one', 'two', 'three', 'four', 'five', 'six']
+    }
     constructor(props) {
         super(props)
         this.state = {die1:'one', die2:'two'}
+        this.roll = this.roll.bind(this)
+    }
+    roll(e) {
+        const randNum1 = Math.floor(Math.random()*this.props.sides.length)
+        const randNum2 = Math.floor(Math.random()*this.props.sides.length)
+        this.setState({die1:this.props.sides[randNum1], die2:this.props.sides[randNum2]})
     }
     render() {
         return (
         <div>
             <Die face={this.state.die1}/>
             <Die face={this.state.die2}/>
+            <button onClick={this.roll}>Roll Dice</button>
         </div>)
 
     }
