35e8559f4376f44b774cd5219189308413856c9b
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-10-29 13:21:47 +0100

    set die.js and link to the awesome fonts in html

---
+++ src/Die.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Die extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+    render() {
+        return (
+            <i className='fas fa-dice-one'></i>
+        )
+    }
+}
+
+export default Die
