208bd14d4f230960af213cbe618c68f85299d554
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-10-29 15:27:14 +0100

    final animation for roll

--- src/Rolldice.js
+++ src/Rolldice.js
@@ -9,22 +9,25 @@ class Rolldice extends Component {
     }
     constructor(props) {
         super(props)
-        this.state = {die1:'one', die2:'two'}
+        this.state = {die1:'one', die2:'two', isRoll:false}
         this.roll = this.roll.bind(this)
     }
     roll(e) {
         const randNum1 = Math.floor(Math.random()*this.props.sides.length)
         const randNum2 = Math.floor(Math.random()*this.props.sides.length)
-        this.setState({die1:this.props.sides[randNum1], die2:this.props.sides[randNum2]})
+        this.setState({isRoll:true})
+        setTimeout(() => {this.setState({die1:this.props.sides[randNum1], die2:this.props.sides[randNum2], isRoll:false})}, 1000)
     }
     render() {
         return (
         <div className='Rolldice'>
             <div className='Rolldice-container'>
-                <Die face={this.state.die1}/>
-                <Die face={this.state.die2}/>
+                <Die face={this.state.die1} isRoll={this.state.isRoll}/>
+                <Die face={this.state.die2} isRoll={this.state.isRoll}/>
             </div>
-            <button onClick={this.roll}>Roll Dice</button>
+            <button onClick={this.roll} disabled={this.state.isRoll} className={`${this.state.isRoll && 'isRoll'}`}>
+            {this.state.isRoll?'Rolling...':'Roll Dice'}
+            </button>
         </div>)
 
     }
